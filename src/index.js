bc982db453022a428d62c5e4dde2bb32c3c0acf7
gilberthappi <gdushimimana6@gmail.com>
2023-08-23 00:18:43 +0200

    Adding HTML, CSS and JS  Files and Folder

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-// import './style.css';
+ import './style.css';
 import changeStatus from './utils.js';
 
-const data = [
-  {
-    description: 'Workout',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Finish today Lessons',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Grocery Shopping',
-    completed: false,
-    index: 3,
-  },
-];
+const storedData = JSON.parse(localStorage.getItem('storedData')) || [];
+
+function updateLocalStorage(data) {
+  localStorage.setItem('storedData', JSON.stringify(data));
+}
 
 function add(data) {
   const listItems = document.getElementById('listItems');
@@ -25,25 +13,71 @@ function add(data) {
   const item = `
     <li class="item" data-index=${data.index}>
       <input  ${checked === true ? 'checked' : ''} type="checkbox" class="checkBox" data-index=${data.index}>
-      <p type="text" contenteditable class="toDoText  ${checked === true ? 'checked' : ''}" data-index=${data.index}>${data.description}</p>
-      <button class="deleteBtn" data-index=${data.index}><i class="far fa-trash-alt" ></i></button>
+      <p contenteditable class="toDoText ${checked === true ? 'checked' : ''}" data-index=${data.index}>${data.description}</p>
+      <button class="deleteBtn" data-index=${data.index}><i class="far fa-trash-alt"></i></button>
     </li>
   `;
   listItems.innerHTML += item;
 }
 
-function initialLoad() {
-  const storedData = JSON.parse(localStorage.getItem('storedData')) || data;
-  if (storedData) {
-    storedData.sort((a, b) => (a.index - b.index))
-      .forEach((data) => {
-        add(data);
-      });
-  }
-  localStorage.setItem('storedData', JSON.stringify(storedData));
-
+function refreshUI() {
   const listItems = document.getElementById('listItems');
-  listItems.addEventListener('change', changeStatus);
+  listItems.innerHTML = ''; // Clear the list
+  storedData.sort((a, b) => (a.index - b.index)).forEach((data) => {
+    add(data);
+  });
 }
 
-initialLoad();
+// Add New Task
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newItemInput = document.getElementById('newItem');
+  const newDescription = newItemInput.value.trim();
+  if (newDescription !== '') {
+    const newIndex = storedData.length + 1;
+    const newTask = {
+      description: newDescription,
+      completed: false,
+      index: newIndex,
+    };
+    storedData.push(newTask);
+    updateLocalStorage(storedData);
+    refreshUI();
+    newItemInput.value = '';
+  }
+});
+
+// Delete Task
+document.getElementById('listItems').addEventListener('click', (e) => {
+  if (e.target.classList.contains('deleteBtn')) {
+    const index = parseInt(e.target.parentElement.dataset.index);
+    const taskIndex = storedData.findIndex(item => item.index === index);
+    if (taskIndex !== -1) {
+      storedData.splice(taskIndex, 1);
+      storedData.forEach((item, idx) => {
+        item.index = idx + 1;
+      });
+      updateLocalStorage(storedData);
+      refreshUI();
+    }
+  }
+});
+
+// Edit Task Description
+document.getElementById('listItems').addEventListener('input', (e) => {
+  if (e.target.classList.contains('toDoText')) {
+    const index = parseInt(e.target.dataset.index);
+    const newDescription = e.target.textContent.trim();
+    const taskIndex = storedData.findIndex(item => item.index === index);
+    if (taskIndex !== -1) {
+      storedData[taskIndex].description = newDescription;
+      updateLocalStorage(storedData);
+    }
+  }
+});
+
+// Checkbox Change
+document.getElementById('listItems').addEventListener('change', changeStatus);
+
+// Initial Load
+refreshUI();
--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,16 @@
 function changeStatus(event) {
-  if (event.target.classList.contains('checkBox')) {
-    const itemText = event.target.parentElement.querySelector('.toDoText');
-    itemText.classList.toggle('checked');
-    itemText.contentEditable = !event.target.checked;
-    const storedData = JSON.parse(localStorage.getItem('storedData'));
-    const { index } = event.target.dataset;
-    const taskIndex = storedData.findIndex((item) => item.index.toString() === index);
-    storedData[taskIndex].completed = event.target.checked;
-    localStorage.setItem('storedData', JSON.stringify(storedData));
+    if (event.target.classList.contains('checkBox')) {
+      const itemText = event.target.parentElement.querySelector('.toDoText');
+      itemText.classList.toggle('checked');
+      itemText.contentEditable = !event.target.checked;
+      const { index } = event.target.dataset;
+      const taskIndex = storedData.findIndex(item => item.index.toString() === index);
+      if (taskIndex !== -1) {
+        storedData[taskIndex].completed = event.target.checked;
+        updateLocalStorage(storedData);
+      }
+    }
   }
-}
-
-export default changeStatus;
+  
+  export default changeStatus;
+  
